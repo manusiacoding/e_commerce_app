d7f4e9deb80e9b23c9990adb341d3fa131636c64
Fawwaz Saputra <manusiacoding29@gmail.com>
2022-09-11 23:33:40 +0700

    add flutter icons

--- lib/screens/product_screen/add_product.dart
+++ lib/screens/product_screen/add_product.dart
@@ -4,24 +4,22 @@ import 'package:saldah_shop/screens/product_screen/list_product.dart';
 class AddProductScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Scaffold(
-        appBar: AppBar(
-          elevation: 0,
-          backgroundColor: Colors.white,
-          centerTitle: true,
-          leading: IconButton(
-              onPressed: () => Navigator.of(context).pushAndRemoveUntil(
-                  MaterialPageRoute(builder: (context) => ListProductScreen()),
-                  (route) => false),
-              icon: Icon(Icons.arrow_back),
-              color: Colors.black),
-        ),
-        body: ListView(
-          children: <Widget>[
-            //
-          ],
-        ),
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 0,
+        backgroundColor: Colors.white,
+        centerTitle: true,
+        leading: IconButton(
+            onPressed: () => Navigator.of(context).pushAndRemoveUntil(
+                MaterialPageRoute(builder: (context) => ListProductScreen()),
+                (route) => false),
+            icon: Icon(Icons.arrow_back),
+            color: Colors.black),
+      ),
+      body: ListView(
+        children: <Widget>[
+          //
+        ],
       ),
     );
   }
--- lib/screens/product_screen/list_product.dart
+++ lib/screens/product_screen/list_product.dart
@@ -12,29 +12,27 @@ class ListProductScreen extends StatefulWidget {
 class _ListProductScreenState extends State<ListProductScreen> {
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Scaffold(
-        appBar: AppBar(
-          elevation: 0,
-          backgroundColor: Colors.white,
-          centerTitle: true,
-          leading: IconButton(
-            onPressed: () => Navigator.of(context).pushAndRemoveUntil(
-                MaterialPageRoute(builder: (context) => HomeScreen()),
-                (route) => false),
-            icon: Icon(Icons.arrow_back),
-            color: Colors.black,
-          ),
-        ),
-        body: Center(
-          child: Text('List Product Screen'),
-        ),
-        floatingActionButton: FloatingActionButton(
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 0,
+        backgroundColor: Colors.white,
+        centerTitle: true,
+        leading: IconButton(
           onPressed: () => Navigator.of(context).pushAndRemoveUntil(
-              MaterialPageRoute(builder: (context) => AddProductScreen()),
+              MaterialPageRoute(builder: (context) => HomeScreen()),
               (route) => false),
-          child: Icon(Icons.add),
+          icon: Icon(Icons.arrow_back),
+          color: Colors.black,
         ),
+      ),
+      body: Center(
+        child: Text('List Product Screen'),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => Navigator.of(context).pushAndRemoveUntil(
+            MaterialPageRoute(builder: (context) => AddProductScreen()),
+            (route) => false),
+        child: Icon(Icons.add),
       ),
     );
   }
